1d5f8da5621d2cc5b3e634e034e78756fb73b4ab
NOOR <noor.s.suliman@gmail.com>
2017-06-15 00:31:41 +0300

    final fixing

--- js/index.js
+++ js/index.js
@@ -1,21 +1,19 @@
-
-        var videoUrl = 'https://vimeo.com/219603868';
-       
-        var endpoint = 'http://www.vimeo.com/api/oembed.json';
-       
-        var callback = 'embedVideo';
-
-        var url = endpoint + '?url=' + encodeURIComponent(videoUrl) + '&callback=' + callback + '&width=640';
-       
-        function embedVideo(video) {
-            document.getElementById('embed').innerHTML = unescape(video.html);
-        }
-       
-        function init() {
-            var js = document.createElement('script');
-            js.setAttribute('type', 'text/javascript');
-            js.setAttribute('src', url);
-            document.getElementsByTagName('head').item(0).appendChild(js);
-        }
+ $(document).ready(function() {
+var stickyNavTop = $('.nav').offset().top;
+ 
+var stickyNav = function(){
+var scrollTop = $(window).scrollTop();
       
-        window.onload = init;
+if (scrollTop > stickyNavTop) { 
+    $('.nav').addClass('sticky');
+} else {
+    $('.nav').removeClass('sticky'); 
+}
+};
+ 
+stickyNav();
+ 
+$(window).scroll(function() {
+  stickyNav();
+});
+});
